9f7ed78fd418eea502506af676e7379a67e86832
codegenius1017 <joem10067@gmail.com>
2017-01-26 15:41:52 +0700

    adding htaccess file

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://www.tokoci.dev/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
